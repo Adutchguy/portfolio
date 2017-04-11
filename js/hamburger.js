adf1944f7b55b35c8aa1b4927c7a74b08443fa23
Adutchguy <mdm_8@hotmail.com>
2017-04-11 13:32:53 -0700

    Got hamburger menu to display and hide on mouseover or mouseleave. Also added the proper linking behavior for the nav menu.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,16 @@
 'use strict'
 
-$(document).ready(function(){
-  var icon = $('.icon-menu');
-  var ul = $('.navigation-list');
-  ul.hide();
-  icon.click(function(event){
-    event.preventDefault();
-    ul.show();
-  })
+var icon = document.getElementById('icon-menu');
+var ul = document.getElementById('navigation-list');
+ul.style.display = 'none';
+icon.addEventListener('mouseover',function(event) {
+  event.preventDefault();
+  icon.style.display = 'none';
+  ul.style.display = 'block';
+})
+
+ul.addEventListener('mouseleave',function(event) {
+  event.preventDefault();
+  icon.style.display = 'block';
+  ul.style.display = 'none';
 })
