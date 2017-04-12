4de0702a802724ed3a60a8695625707708c50196
Adutchguy <mdm_8@hotmail.com>
2017-04-12 13:23:21 -0700

    Got my jQuery to replace the <a>'s href attribute which surrounds the project images with the url's from my project array

--- js/renderProjects.js
+++ js/renderProjects.js
@@ -1,17 +1,6 @@
 'use strict';
 
-var $container = $('.projects');
-var $project1 = $('.project1');
-var $project2 = $('.project2');
-var $project3 = $('.project3');
 var projectObjs = [];
-
-// var renderPictures = function(){
-//   $project1.append("<img class='pro1' src='img/about_me.jpg'>");
-//   $project2.append("<img class='pro2' src='img/cookie_stand.jpg'>");
-//   $project3.append("<img class='pro3' src='img/busmall.jpg'>");
-// }
-// renderPictures();
 
 function Projects(projects) {
   this.title = projects.title,
@@ -19,7 +8,16 @@ function Projects(projects) {
   this.url = projects.url,
   this.date = projects.date
 }
-.
+
 Projects.prototype.toHtml = function() {
   var $newProject = $('.project1')
 }
+
+projects.forEach(function(articleObject) {
+  projectObjs.push(new Projects(articleObject));
+});
+
+$("a.projects").each(function(index,value) {
+  var i = index;
+  $(this).attr('href', projectObjs[i].url);
+})
