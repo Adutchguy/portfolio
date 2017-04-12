dbec59a873f5e52d9769acf41ea518f567c1b7a4
Adutchguy <mdm_8@hotmail.com>
2017-04-12 11:59:53 -0700

    added a carousel for my project images and fixed some CSS issues.

--- js/renderProjects.js
+++ js/renderProjects.js
@@ -4,10 +4,22 @@ var $container = $('.projects');
 var $project1 = $('.project1');
 var $project2 = $('.project2');
 var $project3 = $('.project3');
-renderPictures();
+var projectObjs = [];
 
-var renderPictures = function(){
-  $project1.append("<img class='pro1' src='img/about_me.jpg'>");
-  $project2.append("<img class='pro2' src='img/cookie_stand.jpg'>");
-  $project3.append("<img class='pro3' src='img/busmall.jpg'>");
+// var renderPictures = function(){
+//   $project1.append("<img class='pro1' src='img/about_me.jpg'>");
+//   $project2.append("<img class='pro2' src='img/cookie_stand.jpg'>");
+//   $project3.append("<img class='pro3' src='img/busmall.jpg'>");
+// }
+// renderPictures();
+
+function Projects(projects) {
+  this.title = projects.title,
+  this.author = projects.author,
+  this.url = projects.url,
+  this.date = projects.date
+}
+.
+Projects.prototype.toHtml = function() {
+  var $newProject = $('.project1')
 }
