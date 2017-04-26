b37137f7f144d4c2cc80673858ab913b4aa6ffa5
Adutchguy <mdm_8@hotmail.com>
2017-04-26 10:35:05 -0700

    refactored the functions in hamburger.js to use arrow functions and changed the variables to use let.

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,13 +1,13 @@
 'use strict'
 
-var icon = $('.icon-menu');
-var ul = $('#navigation-list');
+let icon = $('.icon-menu');
+let ul = $('#navigation-list');
 ul.hide();
-icon.on('mouseover',function() {
+icon.on('mouseover', () => {
     icon.hide();
     ul.fadeIn(500);
   })
-ul.on('mouseleave',function(){
+ul.on('mouseleave',() => {
   ul.slideUp(500);
   icon.delay(250).slideDown(500);
 })
