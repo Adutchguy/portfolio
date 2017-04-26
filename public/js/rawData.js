8593e6f9748d1cc5b132425c8e4af60e11ae1f49
Adutchguy <mdm_8@hotmail.com>
2017-04-26 10:43:52 -0700

    cleaned up dead text in rawData.js and changed var to let

--- public/js/rawData.js
+++ public/js/rawData.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var projects = [
+let projects = [
   {
     title:  'Busmall',
     author: 'Michael Miller',
@@ -20,30 +20,3 @@ var projects = [
     date:   '2017-10-03'
   },
 ]
-
-// var education = [
-//   {
-//     organization: 'Code Fellows',
-//     topicOfStudy: 'Full Stack Javascript',
-//     startDate: '2017-03-06',
-//     endDate: 'present'
-//   },
-//   {
-//     organization: 'Edmonds Community College',
-//     topicOfStudy: 'Network Administration',
-//     startDate: '2013',
-//     endDate: '2015'
-//   },
-//   {
-//     organization: 'Skagit Valley College',
-//     topicOfStudy: 'Automotive Technology',
-//     startDate: '404',
-//     endDate: '404'
-//   },
-//   {
-//     organization: 'Everett Community College',
-//     topicOfStudy: 'Fire Fighting',
-//     startDate: '404',
-//     endDate: '404'
-//   }
-// ]
