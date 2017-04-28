390618a68fcc0d35a3dff6462121534dbf297505
Adutchguy <mdm_8@hotmail.com>
2017-04-28 12:16:05 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Load Express
 const express = require('express');
+const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
