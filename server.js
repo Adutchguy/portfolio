e467063e842d5041573f8d036fe98ef3e10967e3
Michael <mdm_8@hotmail.com>
2017-09-15 19:59:58 -0700

    rebuilding portfolio with new file structure and using react. Removed public folder and added new package.json

--- server.js
+++ server.js
@@ -6,13 +6,3 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 app.use(express.static('./public/js/controllers'));
 app.listen(PORT, () => console.log(`Served atlocalhost: ${PORT}`));
-
-function proxyGitHub(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
-  (requestProxy({
-    url: `https://api.github.com/${request.params[0]}`,
-    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
-  }))(request, response);
-}
-
-app.get('/github/*', proxyGitHub);
