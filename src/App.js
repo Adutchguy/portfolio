c664e2f313e41d1fd20780cb69ae2136ad251ee2
Michael <mdm_8@hotmail.com>
2017-09-15 22:38:14 -0700

    added .eslintignore file and gitignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid,Jumbotron} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Portfolio</h1>
+            <p>This is Michael's portfolio.</p>
+          </Grid>
+        </Jumbotron>
       </div>
     );
   }
