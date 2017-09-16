56577f39e1a804c6479379b769c3a6d98b7e6b07
Michael <mdm_8@hotmail.com>
2017-09-15 21:32:35 -0700

    rebuilding using create-react-app as a starting point

--- server.js
+++
@@ -1,8 +0,0 @@
-// Load Express
-const express = require('express');
-const requestProxy = require('express-request-proxy');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static('./public'));
-app.use(express.static('./public/js/controllers'));
-app.listen(PORT, () => console.log(`Served atlocalhost: ${PORT}`));
