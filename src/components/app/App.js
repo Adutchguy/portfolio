7c495611bbd50cfd897ccbb8caa48697a238990a
Michael <mdm_8@hotmail.com>
2017-09-17 20:51:42 -0700

    added a nav and header wireframe

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import {Grid,Jumbotron} from 'react-bootstrap';
+import NavDropdownApp from '../nav';
+import PageHeaderApp from '../header';
 
 class App extends Component {
   render() {
     return (
       <div>
+
+        <NavDropdownApp />
+
+        <PageHeaderApp />
+
         <Jumbotron>
+
           <Grid>
+
             <h1>Portfolio</h1>
+
             <p>This is Michael's portfolio.</p>
+
           </Grid>
         </Jumbotron>
       </div>
---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {PageHeader} from 'react-bootstrap';
+
+const PageHeaderApp = React.createClass({
+  render() {
+    return (
+      <PageHeader>Header Area</PageHeader>
+    );
+  },
+});
+
+export default PageHeaderApp;
