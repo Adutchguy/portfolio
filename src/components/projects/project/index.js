bef288ad3d9f5609c05b61062f53c28af6e00ecc
Michael <mdm_8@hotmail.com>
2017-10-03 23:03:37 -0700

    Some scss tweaking

--- src/components/projects/project/index.js
+++ src/components/projects/project/index.js
@@ -12,7 +12,7 @@ class Project extends React.Component {
     return (
       <div className='project'>
         <header className='project-header'>
-          <a target='_blank' href={this.props.githubRepo.html_url}><h2 className='project-header-h3'>{this.props.githubRepo.name}</h2></a>
+          <h2 className='project-header-h2'>{this.props.githubRepo.name}</h2>
         </header>
         <div className='project-details'>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
